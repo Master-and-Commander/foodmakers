0be98c4bea0e25b48b8f648befe4d4b258f46be3
ISENGRIM\caleb <calebdlaguna@hotmail.com>
2020-06-09 15:09:44 -0400

    small changes

--- client/src/components/home.component.js
+++ client/src/components/home.component.js
@@ -5,162 +5,156 @@ import { Link, Redirect } from "react-router-dom";
 import {useAsyncHookforList} from './useAsyncHookforList';
 
 
-
- const Home = () => {
-   const { setAuthTokens, authTokens } = useAuth();
-   const [userDetails, setUserDetails] = useState();
-   const [depart, setDepart] = useState("false");
-   const [extra, setExtra] = useState("notset");
-   const [buttons, setButtons] = useState([]);
-   const [eatList, setEatlist] = useState([]);
-   const [userReviews, setUserReviews] = useState([]);
-   const [userArticles, setUserArticles] = useState([]);
-   const [userTags, setUserTags] = useState([]);
-   const [description, setDescription] = useState("");
-    
-
-
-   const initializeUserProfile = () => {
-     // pull user basics
-     const profilePromise = axios.get("http://192.168.99.104:6200/api/users/get/" + authTokens[0].usernameID);
-     return profilePromise.then(async(data) => {
-       setUserDetails(data["data"]);
-       setDescription(<p className="card-text">
-        {data["data"]["description"]}
-       </p>)
-       presentUserEatList(data["data"]["toEat"]);
-       presentUserReviews(data["data"]["reviews"]);
-       presentUserArticles(data["data"]["articles"]);
-       presentUserTags(data["data"]["tags"], data["data"] );
-
-
-     })
-     //
-   }
-
-   const presentUserEatList = async(e) => {
-     var arr = [];
+// str to replace : http://192.168.99.104:6200
+const Home = () => {
+  /*
+   Home shows the user's profile description and created articles
+  */
+  const { setAuthTokens, authTokens } = useAuth();
+  const [userDetails, setUserDetails] = useState();
+  const [depart, setDepart] = useState("false");
+  const [extra, setExtra] = useState("notset");
+  const [buttons, setButtons] = useState([]);
+  const [eatList, setEatlist] = useState([]);
+  const [userReviews, setUserReviews] = useState([]);
+  const [userArticles, setUserArticles] = useState([]);
+  const [userTags, setUserTags] = useState([]);
+  const [description, setDescription] = useState("");
+  
+  const initializeUserProfile = () => {
+    // pull user basics from auth tokens username ID
+    const profilePromise = axios.get("http://192.168.99.104:6200/api/users/get/" + authTokens[0].usernameID);
+    return profilePromise.then(async(data) => {
+      setUserDetails(data["data"]);
+      setDescription(<span>
+      {data["data"]["description"]}
+      </span>);
+      presentUserEatList(data["data"]["toEat"]);
+      presentUserReviews(data["data"]["reviews"]);
+      presentUserArticles(data["data"]["articles"]);
+      presentUserTags(data["data"]["tags"], data["data"] );
 
 
+    });
+  }
 
-     Promise.all(e.map(async(item)=> {
-       var author = await axios.get("http://192.168.99.104:6200/api/articles/fetchauthor/" + item);
-       var title = await axios.get("http://192.168.99.104:6200/api/articles/fetchtitle/" + item);
-       var articleInfo = await axios.get("http://192.168.99.104:6200/api/articles/get/" + item);
-       var name = await axios.get("http://192.168.99.104:6200/api/users/fetchusername/" + author["data"]);
-       var path = "/"+name["data"]+"/"+item;
-       console.log("path " + path);
+  const presentUserEatList = async(e) => {
+    // these are 'starred' articles that the user wants to see for further consideration
+    var arr = [];
+    Promise.all(e.map(async(item)=> {
+      var author = await axios.get("http://192.168.99.104:6200/api/articles/fetchauthor/" + item);
+      var title = await axios.get("http://192.168.99.104:6200/api/articles/fetchtitle/" + item);
+      var articleInfo = await axios.get("http://192.168.99.104:6200/api/articles/get/" + item);
+      var name = await axios.get("http://192.168.99.104:6200/api/users/fetchusername/" + author["data"]);
+      var path = "/"+name["data"]+"/"+item;
+      console.log("path " + path);
 
-       arr.push(<div>
-         <div className="card border-0">
-           <div className="card-body">
-           <div className="row no-gutters">
-             <div className="col-md-1">
-             <img src={"/"+name["data"]+"/"+item+".PNG"} className="card-img rounded-0" alt="..." />
-             </div>
+      arr.push(
+      <div>
+        <div className="card border-0">
+          <div className="card-body">
+            <div className="row no-gutters">
+              <div className="col-md-1">
+                <img src={"/"+name["data"]+"/"+item+".PNG"} className="card-img rounded-0" alt="..." />
+              </div>
+            <div className="col-md-8">
+              <div className = "row">
+                <h3 className="card-title" >
+                  <Link to={{pathname: '/article', state: { articleID: item }}}>{title["data"]}</Link> by 
+                  <Link to={{pathname: '/user', state: { userID: author["data"] }}}>{name["data"]}</Link> 
+                </h3>
+                  <div className="dropdown ml-auto">
+                    <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Options
+                    </button>
+                    <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                      <button className="dropdown-item" onClick={() => {setDepart("reviewArticle"); setExtra(item);}}>
+                        Review
+                      </button>
+                      <button className="dropdown-item" onClick={() => {setDepart("request"); setExtra(item)}}>
+                        Request
+                      </button>
+                      <button className="dropdown-item" onClick={() => {
+                        console.log("removing http://localhost:5000/users/removeitemfromeatlist/"+authTokens[0].usernameID+"/"+item);
+                        axios.post("http://192.168.99.104:6200/api/users/removeitemfromeatlist/"+authTokens[0].usernameID+"/"+item);
+                        initializeUserProfile();
+                      }} >
+                        Remove
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <p className="card-text"> Description: {articleInfo["data"]["description"]} </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>);
+    })).then(() => {
+      setEatlist(arr);
+    });
+  }
 
-           <div className="col-md-8">
-             <div className = "row">
-               <h3 className="card-title" ><Link to={{
-                      pathname: '/article',
-                      state: { articleID: item }}}>{title["data"]}</Link> by <Link to={{
-                             pathname: '/user',
-                             state: { userID: author["data"] }}}>{name["data"]}</Link> </h3>
-                             <div className="dropdown ml-auto">
-                               <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                   Options
-                               </button>
-                               <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                 <button className="dropdown-item" onClick={() => {setDepart("reviewArticle"); setExtra(item);}} >Review</button>
-                                 <button className="dropdown-item" onClick={() => {setDepart("request"); setExtra(item)}} >Request</button>
-                                 <button className="dropdown-item" onClick={() => {
-                                   console.log("removing http://localhost:5000/users/removeitemfromeatlist/"+authTokens[0].usernameID+"/"+item);
-                                   axios.post("http://192.168.99.104:6200/api/users/removeitemfromeatlist/"+authTokens[0].usernameID+"/"+item);
-                                   initializeUserProfile();
-                                 }} >Remove</button>
-                               </div>
-                             </div>
-
-             </div>
-              <p className="card-text"> Description: {articleInfo["data"]["description"]} </p>
-
-           </div>
-
-           </div>
-           </div>
-         </div>
-       </div>);
-     })).then(() => {
-        setEatlist(arr);
-     });
-
-     //setEatlist(arr);
-
-   }
-
-   const presentUserReviews = (e) => {
-     // need the article name
-     // need the author of the article name
-     // need the author name
-
-     var arr = [];
-
-     Promise.all(e.map(async(item)=> {
-       var review = await axios.get("http://192.168.99.104:6200/api/reviews/get/"+item);
-
-       console.log(item);
-       console.log("http://192.168.99.104:6200/api/articles/fetchauthor/" + review["data"]["articleId"]);
-       var author = await axios.get("http://192.168.99.104:6200/api/articles/fetchauthor/" + review["data"]["articleId"]);
-
-       var title = await axios.get("http://192.168.99.104:6200/api/articles/fetchtitle/" + review["data"]["articleId"]);
-       console.log("http://192.168.99.104:6200/api/articles/fetchtitle/" + review["data"]["articleId"]);
-       console.log("http://192.168.99.104:6200/api/users/fetchusername/" + author["data"]["username"]);
-       var name = await axios.get("http://192.168.99.104:6200/api/users/fetchusername/" + author["data"]);
-       console.log("approved");
-       console.log(review["data"]["approved"]);
-
-       arr.push(<div>
-         <div className="card border-0">
-           <div className="card-body">
-           <div className="row no-gutters">
-             <div className="col-md-1">
-             <img src={"/"+name["data"]+"/"+review["data"]["articleId"]+".PNG"} className="card-img rounded-0" alt="..." />
-             </div>
-
-           <div className="col-md-8">
-             <div className = "row">
-               <h3 className="card-title" ><Link to={{
-                      pathname: '/article',
-                      state: { articleID: review["data"]["articleId"] }}}>{title["data"]}</Link> by <Link to={{
-                             pathname: '/user',
-                             state: { userID: author["data"] }}}>{name["data"]}</Link> {review["data"]["rating"]}/5  {review["data"]["approved"]}</h3>
-                             <div className="dropdown ml-auto">
-                               <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                   Options
-                               </button>
-                               <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                 <button className="dropdown-item" onClick={() => {
-                                   setDepart("viewreview");
-                                   setExtra(item);
-                                 }} >View</button>
-                                 <button className="dropdown-item" onClick={() => {setDepart("reviewArticle"); setExtra(review["data"]["articleId"])}} >edit</button>
-                               </div>
-                             </div>
-
-             </div>
-              <p className="card-text"> Description: {review["data"]["description"]} </p>
-
-           </div>
-
-           </div>
-           </div>
-         </div>
-       </div>);
-     })).then(() => {
-        setUserReviews(arr);
-     });
-
-   }
+  const presentUserReviews = (e) => {
+    // need the article name
+    // need the author of the article name
+    // need the author name
+    // the array to return
+    var arr = [];
+    Promise.all(e.map(async(item)=> {
+      // Reviews to fetch
+      var review = await axios.get("http://192.168.99.104:6200/api/reviews/get/"+item);
+      var author = await axios.get("http://192.168.99.104:6200/api/articles/fetchauthor/" + review["data"]["articleId"]);
+      var title = await axios.get("http://192.168.99.104:6200/api/articles/fetchtitle/" + review["data"]["articleId"]);
+      var name = await axios.get("http://192.168.99.104:6200/api/users/fetchusername/" + author["data"]);
+      arr.push(
+        <div>
+          <div className="card border-0">
+            <div className="card-body">
+              <div className="row no-gutters">
+                <div className="col-md-1">
+                  <img src={"/"+name["data"]+"/"+review["data"]["articleId"]+".PNG"} className="card-img rounded-0" alt="..." />
+                </div>
+                <div className="col-md-8">
+                  <div className = "row">
+                    <h3 className="card-title" >
+                      <Link to={{pathname: '/article',state: { articleID: review["data"]["articleId"] }}}>
+                        {title["data"]}
+                      </Link> by 
+                      <Link to={{pathname: '/user', state: { userID: author["data"] }}}>
+                        {name["data"]}
+                      </Link> 
+                      {review["data"]["rating"]}/5  {review["data"]["approved"]}
+                    </h3>
+                    <div className="dropdown ml-auto">
+                      <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Options
+                      </button>
+                      <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <button className="dropdown-item" onClick={() => {
+                          setDepart("viewreview");
+                          setExtra(item);
+                        }} >
+                          View
+                        </button>
+                        <button className="dropdown-item" onClick={() => {setDepart("reviewArticle"); setExtra(review["data"]["articleId"])}} >
+                          edit
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                  <p className="card-text"> 
+                    Description: {review["data"]["description"]} 
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>);
+      })).then(() => {
+      setUserReviews(arr);
+    });
+  }
 
    const presentUserArticles = async(e) => {
      var arr = [];
@@ -219,9 +213,6 @@ import {useAsyncHookforList} from './useAsyncHookforList';
 
    const presentUserTags = (e, details) => {
      var arr = [];
-     /// details["data"]["specialties"].includes(item)
-     console.log("details");
-     console.log(details["specialties"]);
      Promise.all(e.map(async(item) => {
        var tag = await axios.get("http://192.168.99.104:6200/api/tags/get/" + item);
        if(details["specialties"].includes(item))
